76d03fa8d7ccba3566f622684feb04ee0475c8d0
Timberlon Gray <graytimberlon@utexas.edu>
2017-03-11 18:43:43 -0600

    Comments/minor changes

--- src/assignment4/Critter1.java
+++ src/assignment4/Critter1.java
@@ -1,7 +1,21 @@
 package assignment4;
+/* CRITTERS Main.java
+ * EE422C Project 4 submission by
+ * Timberlon Gray
+ * tg22698
+ * 16235
+ * Raiyan Chowdhury
+ * rac4444
+ * 16235
+ * Slip days used: <0>
+ * Fall 2016
+ */
 
-public class Critter1 extends Critter { // "runner"
-
+public class Critter1 extends Critter {
+/**
+ * The Runner must always run. It will reprocude only with a significant 
+ * amount of energy headroom.
+ */
 	@Override
 	public void doTimeStep() {
 		if(getEnergy() > 250) {
@@ -10,10 +24,16 @@ public class Critter1 extends Critter { // "runner"
 		}
 		run(getRandomInt(8));
 	}
-
+/**
+ * The critter will always try to run, except in the cases where it is hungry 
+ * and desperate to eat (will die in <3 turns of running) or where it is likely 
+ * to win a fight because of its energy count.
+ */
 	@Override
-	public boolean fight(String opponent) { // will run as characteristic unless it is exceedingly likely he will win,or he is low on energy ("hungry")
-		if ((getEnergy() > 100) || (getEnergy() < Params.run_energy_cost * 3)) return true;
+	public boolean fight(String opponent) {
+		if ((getEnergy() > 100) || (getEnergy() < Params.run_energy_cost * 3)) {
+			return true;
+		}
 		run(getRandomInt(8));
 		return false;
 	}
--- src/assignment4/Critter2.java
+++ src/assignment4/Critter2.java
@@ -1,10 +1,25 @@
 package assignment4;
+/* CRITTERS Main.java
+ * EE422C Project 4 submission by
+ * Timberlon Gray
+ * tg22698
+ * 16235
+ * Raiyan Chowdhury
+ * rac4444
+ * 16235
+ * Slip days used: <0>
+ * Fall 2016
+ */
 
-public class Critter2 extends Critter { // "sloth"
+public class Critter2 extends Critter {
 	private int slowMove = 0; // moves every 6 turns
+/**
+ * The Sloth moves very infrequently, and reproduces at a 
+ * fairly low energy count.
+ */
 	@Override
 	public void doTimeStep() {
-		if(getEnergy() == 40) {
+		if(getEnergy() > 40) {
 			Critter2 child = new Critter2();
 			reproduce(child, Critter.getRandomInt(8));
 		}
@@ -14,8 +29,12 @@ public class Critter2 extends Critter { // "sloth"
 		} else slowMove--;
 	}
 
+/**
+ * Because the sloth is vegetarian, it will try to run from any 
+ * fight that is not with algae.
+ */
 	@Override
-	public boolean fight(String opponent) { // only "fights" plants
+	public boolean fight(String opponent) {
 		if (opponent.equals("@")) return true;
 		return false;
 	}
